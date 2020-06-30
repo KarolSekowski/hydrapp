128ee02e17af2b2e260afb302bf0d05858921cbf
Karol Sekowski <sekowskikarol@hotmail.com>
2020-06-30 22:13:18 +0200

    counter plus and minus added, also minimum zero rule

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,24 @@ registerSW();
 
 /* place your code below */
 
+const add = document.querySelector('.plus_button--js');
+const remove = document.querySelector('.minus_button--js');
+const counter = document.querySelector('.glass_counter--js')
+let countResult = 0;
+
+
+add.addEventListener('click', ()=> {
+    countResult++;
+    counter.innerHTML= countResult;
+    });
+
+
+    remove.addEventListener('click',()=>{
+        if (countResult > 0){
+    countResult--;
+    counter.innerHTML=countResult;
+        }
+    });
+   
+
+  
