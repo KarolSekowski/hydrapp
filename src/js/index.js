9f64b9ff14fb178d50782f44b8782623ee37a28c
Karol Sekowski <sekowskikarol@hotmail.com>
2020-06-30 22:44:41 +0200

    local storage memory added

--- src/js/index.js
+++ src/js/index.js
@@ -10,20 +10,37 @@ const add = document.querySelector('.plus_button--js');
 const remove = document.querySelector('.minus_button--js');
 const counter = document.querySelector('.glass_counter--js')
 let countResult = 0;
+const key = new Date().toISOString().slice(0,10);
+
+const localStorageValue = localStorage.getItem(key);
+
+    if (localStorageValue){
+        countResult = localStorageValue;
+}
+    else{
+        localStorage.setItem(key, 0);
+    
+}
+counter.innerHTML = countResult;
+
 
 
 add.addEventListener('click', ()=> {
-    countResult++;
-    counter.innerHTML= countResult;
+        countResult++;
+        counter.innerHTML= countResult;
+        localStorage.setItem(key, countResult);
     });
 
 
-    remove.addEventListener('click',()=>{
+remove.addEventListener('click',()=>{
         if (countResult > 0){
-    countResult--;
-    counter.innerHTML=countResult;
+            countResult--;
+    
         }
-    });
+        counter.innerHTML=countResult;
+        localStorage.setItem(key, countResult);
+});
+
    
 
   
